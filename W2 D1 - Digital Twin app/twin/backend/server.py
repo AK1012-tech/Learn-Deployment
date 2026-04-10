b4424705802368a1a9577e3dfafa9aea8d4e2a1b
Ambar Kumar <ambarkumar1012@gmail.com>
2026-04-10 12:03:14 +0530

    modify gitignore

--- W2 D1 - Digital Twin app/twin/backend/server.py
+++ W2 D1 - Digital Twin app/twin/backend/server.py
@@ -29,11 +29,11 @@ app.add_middleware(
 # Initialize Bedrock client - see Q42 on https://edwarddonner.com/faq if the Region gives you problems
 bedrock_client = boto3.client(
     service_name="bedrock-runtime", 
-    region_name=os.getenv("DEFAULT_AWS_REGION", "us-east-1")
+    region_name=os.getenv("DEFAULT_AWS_REGION", "ap-south-1")
 )
 
 # Bedrock model selection - see Q42 on https://edwarddonner.com/faq for more
-BEDROCK_MODEL_ID = os.getenv("BEDROCK_MODEL_ID", "global.amazon.nova-2-lite-v1:0")
+BEDROCK_MODEL_ID = os.getenv("BEDROCK_MODEL_ID", "apac.amazon.nova-lite-v1:0")
 
 # Memory storage configuration
 USE_S3 = os.getenv("USE_S3", "false").lower() == "true"
